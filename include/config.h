c3c92b3da81e35d551d1862a163485b43a79eb76
Honza Kubita <honza.kubita@gmail.com>
2023-11-23 11:10:19 +0100

    Added config file

---
+++ include/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const int MOT_A_DIR = 12;
+const int MOT_A_SPEED = 3;
+const int MOT_A_BREAK = 9;
+const int MOT_B_DIR = 13;
+const int MOT_B_SPEED = 11;
+const int MOT_B_BREAK = 8;
+
+const int EMG1 = 0;
+const int EMG2 = 1;
+
+#endif // CONFIG_H
